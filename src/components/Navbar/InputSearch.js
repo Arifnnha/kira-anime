c837ad80be6a50f3df2094809dd8ba262985c224
Arif <muhamadarifulinnuha@gmail.com>
2024-11-17 09:01:00 +0700

    "Update InputSearch component: add 'position-relative' class to div, adjust button position and class"

--- src/components/Navbar/InputSearch.js
+++ src/components/Navbar/InputSearch.js
@@ -4,9 +4,9 @@ import { MagnifyingGlass } from "@phosphor-icons/react";
 
 const InputSearch = () => {
   return (
-    <div>
+    <div className="position-relative">
       <input placeholder="cari anime..." className="p-2 rounded" />
-      <button className="absolute top-6 end-5">
+      <button className="absolute top-2 end-2">
         <MagnifyingGlass size={26} />
       </button>
     </div>
