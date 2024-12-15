3f310f674fbe1f2435a9d22aebfd4171ea4ae5b7
Arifnnha <muhamadarifulinnuha@gmail.com>
2024-12-15 16:40:30 +0700

    validate page dashboard must sign in(tugas cuyuniverse)

--- src/app/users/dashboard/page.jsx
+++ src/app/users/dashboard/page.jsx
@@ -3,13 +3,12 @@ import Image from "next/image";
 
 const Page = async () => {
   const user = await authUserSession();
-  console.log(user);
 
   return (
     <div className="text-color-primary">
       <h3>dashboard</h3>
-      <h5>Welcome, {user.name}</h5>
-      <Image src={user.image} alt={user.name} width={250} height={250} />
+      <h5>Welcome, {user?.name}</h5>
+      <Image src={user?.image} alt={user?.name} width={250} height={250} />
     </div>
   );
 };
